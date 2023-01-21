39996084f6d5309e0461e3d3529a418cbb1f825f
Billy_coding <billy.rogier@free.fr>
2023-01-21 10:30:44 +0100

    update

--- assets/js/animation_project.js
+++ assets/js/animation_project.js
@@ -1,1 +1,32 @@
-(document.onreadystatechange=function (){"complete" !=document.readyState ? ((lodaer=!0), (circle.style.top=0), (circle.style.left=0), (loader.style.opacity=1)) : setTimeout(function (){(circle.style.transition="1s"), circle.classList.remove("active"), circle.classList.add("disaper"), (loader.style.opacity=0), setTimeout(function (){(main.style.opacity=1), (navbar.style.opacity=1), (footer.style.opacity=1), (lodaer=!1), (body.style.overflowY="scroll"), window.innerWidth > 768, reveal(0), slide_percentage();}, 600);}, 1e3);}), menu.addEventListener("click", menuAnimation);for (var i=0; i < links.length; i++) 4==i && links[i].addEventListener("click", menuAnimation);window.addEventListener("scroll", function (){!0 !=lodaer && reveal(200);});window.addEventListener("resize", function (){reveal(0);});
+(document.onreadystatechange = function () {
+    "complete" != document.readyState
+        ? ((lodaer = !0),
+          (circle.style.top = 0),
+          (circle.style.left = 0),
+          (loader.style.opacity = 1))
+        : setTimeout(function () {
+              (circle.style.transition = "1s"),
+                  circle.classList.remove("active"),
+                  circle.classList.add("disaper"),
+                  (loader.style.opacity = 0),
+                  setTimeout(function () {
+                      (main.style.opacity = 1),
+                          (navbar.style.opacity = 1),
+                          (footer.style.opacity = 1),
+                          (lodaer = !1),
+                          (body.style.overflowY = "scroll"),
+                          window.innerWidth > 768,
+                          reveal(0),
+                          slide_percentage();
+                  }, 600);
+          }, 1e3);
+}),
+    menu.addEventListener("click", menuAnimation);
+for (var i = 0; i < links.length; i++)
+    4 == i && links[i].addEventListener("click", menuAnimation);
+window.addEventListener("scroll", function () {
+    !0 != lodaer && reveal(200);
+});
+window.addEventListener("resize", function () {
+    reveal(0);
+});
--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,1 +1,50 @@
-const d=new Date();d.setTime(d.getTime() + 1296e6);let expires=";path=/" + ";expires=" + d.toUTCString();function getCookie(e){return (deb=document.cookie.indexOf(e + "=")) >=0 ? ((deb +=e.length + 1), (fin=document.cookie.indexOf(";", deb)) < 0 && (fin=document.cookie.length), document.cookie.substring(deb, fin)) : null;}const darkLightToggle=document.getElementById("toggle_light");function cookieMode(){let e=document.getElementById("logo"); "on"==getCookie("mode") ? (document.documentElement.style.setProperty( "--background-color", "#E9E9E9" ), document.documentElement.style.setProperty("--text-color", "#050b29"), document.documentElement.style.setProperty( "--light-color", "#686464" ), (e.style.color="#050b29")) : (document.documentElement.style.setProperty( "--background-color", "#050b29" ), document.documentElement.style.setProperty( "--light-color", "#bcb6b6" ), document.documentElement.style.setProperty("--text-color", "#E9E9E9"), (e.style.color="#fff"));}window.addEventListener("load", cookieMode), darkLightToggle.addEventListener("click", ()=>{"on"==getCookie("mode") ? (document.cookie="mode=off;" + expires) : (document.cookie="mode=on;" + expires), cookieMode();});const language=document.getElementById("language");null !=getCookie("lang") || (document.cookie="lang=fr;" + expires), language.addEventListener("click", ()=>{"fr" !=getCookie("lang") ? (document.cookie="lang=fr;" + expires) : (document.cookie="lang=en;" + expires);});
+const d = new Date();
+d.setTime(d.getTime() + 1296e6);
+let expires = ";path=/" + ";expires=" + d.toUTCString();
+function getCookie(e) {
+    return (deb = document.cookie.indexOf(e + "=")) >= 0
+        ? ((deb += e.length + 1),
+          (fin = document.cookie.indexOf(";", deb)) < 0 &&
+              (fin = document.cookie.length),
+          document.cookie.substring(deb, fin))
+        : null;
+}
+const darkLightToggle = document.getElementById("toggle_light");
+function cookieMode() {
+    let e = document.getElementById("logo");
+    "on" == getCookie("mode")
+        ? (document.documentElement.style.setProperty(
+              "--background-color",
+              "#E9E9E9"
+          ),
+          document.documentElement.style.setProperty("--text-color", "#050b29"),
+          document.documentElement.style.setProperty(
+              "--light-color",
+              "#686464"
+          ),
+          (e.style.color = "#050b29"))
+        : (document.documentElement.style.setProperty(
+              "--background-color",
+              "#050b29"
+          ),
+          document.documentElement.style.setProperty(
+              "--light-color",
+              "#bcb6b6"
+          ),
+          document.documentElement.style.setProperty("--text-color", "#E9E9E9"),
+          (e.style.color = "#fff"));
+}
+window.addEventListener("load", cookieMode),
+    darkLightToggle.addEventListener("click", () => {
+        "on" == getCookie("mode")
+            ? (document.cookie = "mode=off;" + expires)
+            : (document.cookie = "mode=on;" + expires),
+            cookieMode();
+    });
+const language = document.getElementById("language");
+null != getCookie("lang") || (document.cookie = "lang=fr;" + expires),
+    language.addEventListener("click", () => {
+        "fr" != getCookie("lang")
+            ? (document.cookie = "lang=fr;" + expires)
+            : (document.cookie = "lang=en;" + expires);
+    });
