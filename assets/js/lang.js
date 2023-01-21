e11b1d474b0f79227ca494d7288e67e424464f05
Billy_coding <billy.rogier@free.fr>
2023-01-21 10:27:37 +0100

    update

--- assets/js/lang.js
+++
@@ -1,7 +0,0 @@
-const language = document.getElementById("language");
-null != getCookie("lang") || (document.cookie = "lang=fr;" + expires),
-    language.addEventListener("click", () => {
-        "fr" != getCookie("lang")
-            ? (document.cookie = "lang=fr;" + expires)
-            : (document.cookie = "lang=en;" + expires);
-    });
