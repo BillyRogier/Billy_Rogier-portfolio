84887820f2d001136f7b7b26ad78c97ba74230ff
Billy Rogier <billy.rogier@free.fr>
2023-05-27 17:54:02 +0200

    add Les Truelles Ardéchoises project

--- contact_form.php
+++ contact_form.php
@@ -1,16 +1,32 @@
 <?php
 
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require './PHPMailer/vendor/autoload.php';
+
 if (isset($_POST)) {
-    $name = $_REQUEST['name'];
-    $email = $_REQUEST['email'];
-    $message = $_REQUEST['message'];
-
-    $to = 'billy.rogier@free.fr';
-    $subject = 'Contact Request From Website';
-    $headers = "From: " . $name . " <" . $email . "> \r\n";
-    $send_email = mail($to, $subject, $message, $headers);
-
-    echo ($send_email) ? 'success' : 'error';
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->SMTPDebug = 0;
+    $mail->Host = "smtp.free.fr";
+    $mail->Port = "465";
+    $mail->SMTPAuth = true;
+    $mail->CharSet = "UTF-8";
+    $mail->Username = "billy.rogier@free.fr";
+    $mail->Password = "Billy007";
+    $mail->SMTPSecure = "ssl";
+    $mail->setFrom($_REQUEST['email'], $_REQUEST['name']);
+    $mail->addReplyTo($_REQUEST['email'], $_REQUEST['name']);
+    $mail->addAddress("billy.rogier@free.fr", ("Billy Rogier"));
+    $mail->Subject = 'Message envoyé via le site internet';
+    $mail->msgHTML(file_get_contents('message.html'), __DIR__);
+    $mail->Body =  $_REQUEST['message'];
+    if (!$mail->send()) {
+        echo 'error';
+    } else {
+        echo 'success';
+    }
 } else {
     header("location: 404");
     exit;
